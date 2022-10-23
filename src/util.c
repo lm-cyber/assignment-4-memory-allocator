3696b81ce657f67d317ebba2a9f81ca50d1152d5
Alan <tyujn24@mail.ru>
2022-10-23 23:02:56 +0300

    Removed unused stuff

--- src/util.c
+++ src/util.c
@@ -5,10 +5,10 @@
 #include "util.h"
 
 _Noreturn void err( const char* msg, ... ) {
-  va_list args;
-  va_start (args, msg);
-  vfprintf(stderr, msg, args);
-  va_end (args);
+  //va_list args;
+  //va_start (args, msg);
+  //vfprintf(stderr, msg, args);
+  //va_end (args);
   abort();
 }
 
