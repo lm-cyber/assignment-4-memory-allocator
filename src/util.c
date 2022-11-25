54ba10e301fd9b8bea3187a9e97a5463aca48375
Alan <tyujn24@mail.ru>
2022-11-25 15:23:20 +0300

    Saved

--- src/util.c
+++ src/util.c
@@ -5,18 +5,10 @@
 #include "util.h"
 
 _Noreturn void err( const char* msg, ... ) {
-<<<<<<< HEAD
-  (void) msg;
-  //va_list args;
-  //va_start (args, msg);
-  //vfprintf(stderr, msg, args);
-  //va_end (args);
-=======
   va_list args;
   va_start (args, msg);
   vfprintf(stderr, msg, args); // NOLINT 
   va_end (args);
->>>>>>> 5358facd8db9f6b1f499912442843d799add16b8
   abort();
 }
 
